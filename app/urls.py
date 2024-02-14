5b0f3227f8dcc9746f656a5ccfec904d9d756de8
salergame <salergame07@gmail.com>
2024-02-14 17:43:36 +0600

    0.4

--- app/urls.py
+++ app/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from logging import DEBUG
+
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+from app import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,7 +27,9 @@ urlpatterns = [
     path('catalog/',include('goods.urls', namespace='catalog')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls"))
+        path("__debug__/", include("debug_toolbar.urls")),
+        
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import render
 from goods.models import Categories
 # Create your views here.
 def index(request):
-    
-    categories = Categories.objects.all()
+
     
     context = {
         'title':'Home - Главная',
         'content': 'Магазин мебели HOME',
-        'categories': categories
+        'categories': Categories
     }
     
     return render(request, 'main/index.html',context)
