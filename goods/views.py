7203c0d3ca24d97ad35e4406b64f3838b5a5f355
salergame <salergame07@gmail.com>
2024-02-14 18:29:03 +0600

    0.5.5

--- goods/views.py
+++ goods/views.py
@@ -4,17 +4,21 @@ from goods.models import Products
 
 # Create your views here.
 
-def catalog(request):
+def catalog(request,category_slug):
     
-    goods = Products.objects.all()
-    
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+        
+            
     context = {
         'title': "Home-Каталог",
         'goods': goods,
     }
     return render(request,"goods/catalog.html",context) 
 
-def product(request, product_slug=False):
+def product(request, product_slug):
     
     product = Products.objects.get(slug=product_slug)
     
