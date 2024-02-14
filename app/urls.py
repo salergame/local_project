f7ab4e96f90312b2fa96c3506746b0f7deacaea5
salergame <salergame07@gmail.com>
2024-02-14 16:05:04 +0600

    0.3.5

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from logging import DEBUG
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,3 +23,8 @@ urlpatterns = [
     path('',include('main.urls', namespace='main')),
     path('catalog/',include('goods.urls', namespace='catalog')),
 ]
+
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls"))
+    ]
