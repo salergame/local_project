3442b94cda9d49a2458a69686f4ca13cee887011
salergame <salergame07@gmail.com>
2024-02-15 16:28:25 +0600

    0.9

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.http import urlencode
+
 
 from goods.models import Categories
 
@@ -9,3 +11,11 @@ register = template.Library()
 @register.simple_tag
 def tag_categories():
     return Categories.objects.all()
+
+
+
+@register.simple_tag(takes_context=True)
+def changes_params(context,**kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
