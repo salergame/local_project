f1fb858ad6dd7e9b86ef23a0c89995e676db79f4
salergame <salergame07@gmail.com>
2024-02-16 12:39:12 +0600

    0.12

--- goods/utils.py
+++ goods/utils.py
@@ -1,6 +1,12 @@
 
 from django.db.models import Q
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import (
+    SearchQuery, 
+    SearchRank, 
+    SearchVector, 
+    SearchHeadline,
+)
+
 from goods.models import Products
 
 
@@ -8,9 +14,33 @@ def q_search(query):
     
     if query.isdigit() and len(query) <=5:
         return Products.objects.filter(id=int(query))
+    
     vector = SearchVector("name","description")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+    result = result.annotate(
+        headline = SearchHeadline(
+            "name",
+            query,
+            start_sel = '<span style="background-color: yellow;">',
+            stop_sel="</span>"
+        )
+    )
+    result = result.annotate(
+        bodyline = SearchHeadline(
+            "description",
+            query,
+            start_sel = '<span style="background-color: yellow;">',
+            stop_sel="</span>"
+        )
+    )
+    return result
+    
 
     
     
