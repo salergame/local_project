291bf38c0c12d9df5d1a09a13b7df3a60f052c7c
salergame <salergame07@gmail.com>
2024-02-20 15:49:25 +0600

    0.14

--- users/forms.py
+++ users/forms.py
@@ -5,19 +5,27 @@ from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    
-    username = forms.CharField(
-        label = 'Имя',
-        widget=forms.TextInput(attrs={"autofocus": True,
-                                      'class': 'form-control',
-                                      'placeholder': 'Введите имя пользователя'})
-    )
-    password = forms.CharField(
-        label = 'Пароль',
-        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-                                          'class': 'form-control',
-                                          'placeholder': 'Введите ваш пароль'})
-    )
     class Meta:
         model = User
         fields = ['username', 'password']
+    
+    username = forms.CharField()
+    password = forms.CharField()
+    
+    
+    
+    
+    
+    
+    # username = forms.CharField(
+    #     label = 'Имя',
+    #     widget=forms.TextInput(attrs={"autofocus": True,
+    #                                   'class': 'form-control',
+    #                                   'placeholder': 'Введите имя пользователя'})
+    # )
+    # password = forms.CharField(
+    #     label = 'Пароль',
+    #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+    #                                       'class': 'form-control',
+    #                                       'placeholder': 'Введите ваш пароль'})
+    # )
