5ffd90c43afbe1f326c406078f14a055296a05b7
salergame <salergame07@gmail.com>
2024-02-22 15:20:49 +0600

    0.19

--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -2,10 +2,10 @@ from django import template
 
 from carts.models import Cart
 
-register =template.Library()
+register = template.Library()
 
 @register.simple_tag()
 def user_carts(request):
-    if request.user.in_authenticated:
+    if request.user.is_authenticated:
         return Cart.objects.filter(user=request.user)
     
